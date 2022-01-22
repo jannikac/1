2ec441fbf59d66b816e46490bf61d10eba34f7f1
jannikac <jannikac@mailbox.org>
2022-01-22 16:12:22 +0100

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 
 class window {
-  public:
-    int width;
-    int height;
-    window(int x, int y) {
-      height = x;
-      width = y;
-    };
-    bool exit();
+public:
+  int width;
+  int height;
+  window(int x, int y) {
+    height = x;
+    width = y;
+  };
+  bool exit();
 };
 
-bool window::exit() {
-  return true;
-}
+bool window::exit() { return true; }
 
-void draw_something(window w, int *b) {};
+void draw_something(window w, int *b){};
 
-void run()
-{
-  int* a = new int(5);
-  int* b;
+void run() {
+  int *a = new int(5);
+  int *b;
   window w(1920, 1080);
   while (!w.exit()) {
     b = new int(1);
@@ -29,7 +26,22 @@ void run()
   delete b;
 }
 
-int main()
-{
-  run();
+void c3() {
+  float f = 5.0f;
+  float *ptr_f = &f;
+  int *ptr_i = (int *)&f;
+
+  std::cout << "f hat Hex-Wert: " << std::hex << *ptr_i;
+  *ptr_f = 10.0f;
+  std::cout << "f hat Hex-Wert: " << std::hex << *ptr_i;
 }
+
+int &dead_meat() {
+  int *meat = new int(15);
+  delete meat;
+  return *meat;
+}
+
+void print_meat(const int &meat) { std::cout << "Always valid: " << meat; }
+
+int main() { return 0; }
